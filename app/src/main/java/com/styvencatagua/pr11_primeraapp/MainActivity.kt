f476c6bc84bfaa37849db25d35f60614450a9f65
styven0004 <styvencatagua@gmail.com>
2026-09-22 21:02:46 +0200

    Assignació botó.

--- app/src/main/java/com/styvencatagua/pr11_primeraapp/MainActivity.kt
+++ app/src/main/java/com/styvencatagua/pr11_primeraapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.styvencatagua.pr11_primeraapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val b = findViewById<Button>(R.id.button)
+
+        b.setOnClickListener {
+            val text = "Hello toast!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText( this, text, duration)
+            toast.show()
+        }
+
     }
 }
